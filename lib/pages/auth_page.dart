5446cda4350992400b5526170b049d54d165315d
DegasiPilot <timurnig@gmail.com>
2024-03-16 08:05:40 +0300

    Add CircularProgressIndicator

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -13,108 +13,121 @@ class _AuthpageState extends State<Authpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-      Center(
-        child:SingleChildScrollView( 
-        child:  Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('images/logo.png',width: MediaQuery.of(context).size.width*0.5, height: MediaQuery.of(context).size.height*0.3,),
-            SizedBox(
-              width: MediaQuery.of(context).size.width*0.9,
-              child: TextField(
-                cursorColor: Colors.white,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Email',
-                  hintStyle:const TextStyle(
-                    color: Colors.white54,
-                  ),
-                  labelStyle:const TextStyle(
-                    color: Colors.white,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide:const BorderSide(color:Colors.white),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide:const BorderSide(color:Colors.white),
-                  ),
-                  prefixIcon:const Icon(
-                    Icons.email,
-                    color: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/logo.png',
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.3,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: TextField(
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Email',
+                    hintStyle: const TextStyle(
+                      color: Colors.white54,
+                    ),
+                    labelStyle: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.email,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height*0.03,
-            ),
-             SizedBox(
-              width: MediaQuery.of(context).size.width*0.9,
-              child: TextField(
-                obscureText: !visibility,
-                cursorColor: Colors.white,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Password',
-                  hintStyle:const TextStyle(
-                    color: Colors.white54,
-                  ),
-                  labelStyle:const TextStyle(
-                    color: Colors.white,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide:const BorderSide(color:Colors.white),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide:const BorderSide(color:Colors.white),
-                  ),
-                  prefixIcon:const Icon(
-                    Icons.password,
-                    color: Colors.white,
-                  ),
-                  suffixIcon: 
-                    IconButton(
-                      icon: !visibility ?
-                     const Icon(Icons.visibility):
-                     const Icon(Icons.visibility_off),
-                      onPressed: (){
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: TextField(
+                  obscureText: !visibility,
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Password',
+                    hintStyle: const TextStyle(
+                      color: Colors.white54,
+                    ),
+                    labelStyle: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.password,
+                      color: Colors.white,
+                    ),
+                    suffixIcon: IconButton(
+                      icon: !visibility
+                          ? const Icon(Icons.visibility)
+                          : const Icon(Icons.visibility_off),
+                      onPressed: () {
                         setState(() {
                           visibility = !visibility;
                         });
                       },
                     ),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height*0.03,
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height*0.06,
-              width: MediaQuery.of(context).size.width*0.55,
-              child: ElevatedButton(onPressed:(){
-                Navigator.popAndPushNamed(context, '/home');
-              } ,
-              child:const Text('Войти') ,),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height*0.03,
-            ),
-            InkWell(
-              highlightColor: Colors.white,
-              onTap: (){
-                Navigator.popAndPushNamed(context, '/reg');
-              },
-              child:const Text('Нет аккаунта? Регистрация'),
-            )
-          ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.06,
+                width: MediaQuery.of(context).size.width * 0.55,
+                child: ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        });
+                    Future.delayed(const Duration(seconds: 5), () {
+                      Navigator.popAndPushNamed(context, '/home');
+                    });
+                  },
+                  child: const Text('Войти'),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              InkWell(
+                highlightColor: Colors.white,
+                onTap: () {
+                  Navigator.popAndPushNamed(context, '/reg');
+                },
+                child: const Text('Нет аккаунта? Регистрация'),
+              )
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
